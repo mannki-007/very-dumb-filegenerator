d47f57f0b62784173284ee3767271c50283b8601
mannki-007 <dayzerpc12@gmail.com>
2026-01-29 00:03:19 +0300

    Added a path for files and label
    path - just type your path where would you save a file (like a /home/user/)
    label - watching for path for files

--- prog.py
+++ prog.py
@@ -1,35 +1,65 @@
 import customtkinter as ctk
+from pathlib import Path
+import os
 
-# функции
 
+# var / переменные
 dot = '.'
-namefile = 'filesmthng'
+namefile = 'type file name and ext'
+namefilepath = 'type path'
 
+
+file_path = '' 
+
+
+# функции / fucntions
 def entry_callback():
     text = entry.get()
 
     button_callback(text)
 
+def entry_path():
+    pathfile = entrypath.get()
 
+    button_callback(pathfile)
+
+fullpath = ''
     
-def button_callback(text):
-    with open(text, "w") as f:
+def button_callback(text, pathfile):
+    fullpath = os.path.join(pathfile, text)
+    with open(fullpath, "w") as f:
         f.write("")
+        print('file created at: ', fullpath)
+        label.configure(text=f'Created at: {fullpath}')
 
 
 
-app = ctk.CTk()
-app.geometry("400x150")
 
 
-# поле ввода
-entry = ctk.CTkEntry(app, placeholder_text=namefile)
+
+# window parameters
+main = ctk.CTk()
+main.geometry("400x200")
+main.title("File creater by mannki")
+
+
+# текст / label
+
+label = ctk.CTkLabel(main,text="", fg_color="transparent")
+label.pack(padx=20, pady=0)
+
+# поле ввода / entry
+entry = ctk.CTkEntry(main, placeholder_text=namefile)
 entry.bind("<Return>", lambda event: entry_callback())
 entry.pack(padx=20, pady=20)
 
-# кнопка
-button = ctk.CTkButton(app, text="my button", command=lambda: button_callback(entry.get()))
+entrypath = ctk.CTkEntry(main, placeholder_text=namefilepath)
+entrypath.bind("<Return>", lambda event: entry_path())
+entrypath.pack(padx=20, pady=0)
+
+# кнопка / buttton
+button = ctk.CTkButton(main, text="Create file!", command=lambda: button_callback(entry.get(), entrypath.get()))
 button.pack(padx=20, pady=20)
 
 
-app.mainloop()
+main.mainloop()
